e9b28c8ec66511aabd9a28b72beee9878769c1aa
Jelson J <jelson20@outlook.com>
2022-02-17 16:59:50 +0000

    add globalStyles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/Global';
+
+const theme = {
+	colors: {
+		primary: '#64257e44',
+		secondary: '#471258'
+	}
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/Global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  
+  *{
+    margin:0;
+    padding: 0;
+    box-sizing: border-box
+  }
+
+  body{
+    color: #f6f4f4;
+    background: #c55656;
+  }
+
+`;
+
+export default GlobalStyle;
